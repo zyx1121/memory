7592197d5f0f0436ab6a85a096e03ec1bc99d716
Loki <yongxiang.zhan@outlook.com>
2024-10-13 21:14:36 +0800

    style: :lipstick: import fira code font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+// Import the font
+import { Fira_Code } from 'next/font/google'
+
+// Initialize the font
+const firaCode = Fira_Code({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: "🐟",
@@ -14,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh-TW" suppressHydrationWarning>
-      <body className="antialiased">
+      <body className={`antialiased ${firaCode.className}`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
